4cc0ea02d373d7e8aeed4ee5a02a919de61ce670
alexanderws <alex.ws@icloud.com>
2023-01-17 10:20:17 +0100

    Initial Commit

---
+++ CameraApp/CameraAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CameraAppApp.swift
+//  CameraApp
+//
+//  Created by Alexander Wiig Sørensen on 17/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct CameraAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
